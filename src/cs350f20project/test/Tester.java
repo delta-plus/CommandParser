b8baf114ec76421227432d308c6dff653545a6d5
jackson-newland <jackson.newland@outlook.com>
2020-11-24 01:13:58 -0800

    Update Tester.java

--- src/cs350f20project/test/Tester.java
+++ src/cs350f20project/test/Tester.java
@@ -3,8 +3,15 @@ package cs350f20project.test;
 import cs350f20project.Startup;
 
 public class Tester {
+<<<<<<< Updated upstream
   public static void main(String args[]) {
     Startup test = new Startup();
     test.go();
   }
+=======
+    public static void main(String args[]) {
+        Startup test = new Startup();
+        test.go();
+    }
+>>>>>>> Stashed changes
 }
